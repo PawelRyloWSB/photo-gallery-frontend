77185a43c97a9dac2225d373190603bfad6000d1
rylcior <rylcior@gmail.com>
2023-03-30 20:43:06 +0200

    Add required packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {router} from "./router";
+import {RouterProvider} from "react-router-dom";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {LocalizationProvider} from "@mui/x-date-pickers";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router}>
-  </React.StrictMode>,
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <RouterProvider router={router} />
+      </LocalizationProvider>
+  </React.StrictMode>
 )
